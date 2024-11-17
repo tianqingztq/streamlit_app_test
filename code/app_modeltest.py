2c33042d83f988af27398d595359472ed5106608
Tianqing <63912134+tianqingztq@users.noreply.github.com>
2024-11-16 21:36:32 -0500

    Update app_modeltest.py

--- code/app_modeltest.py
+++ code/app_modeltest.py
@@ -6,6 +6,7 @@ import pickle
 
 # Create connection object and retrieve file contents.
 conn = st.connection('gcs', type=FilesConnection)
+st.write(conn)
 # with conn.open('streamlit-bucket-tq/rf_model.pkl', 'rb') as file:
 #     rf_model = pickle.load(file)
 rfmodel = conn.read("streamlit-bucket-tq/rf_model.pkl", input_format='None', ttl=600) # cache the result for 600 seconds.
