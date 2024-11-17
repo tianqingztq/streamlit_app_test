031c2b1257ef3ada3334c488393aae1a2b26f2b0
Tianqing <63912134+tianqingztq@users.noreply.github.com>
2024-11-16 21:22:25 -0500

    Add files via upload

---
+++ code/app_modeltest.py
@@ -0,0 +1,11 @@
+# streamlit_app.py
+
+import streamlit as st
+from st_files_connection import FilesConnection
+import pickle
+
+# Create connection object and retrieve file contents.
+conn = st.connection('gcs', type=FilesConnection)
+rfmodel = conn.read("streamlit-bucket-tq/rf_model.pkl", input_format="pkl", ttl=600) # cache the result for 600 seconds.
+rf_model = pickle.load(rfmodel)
+st.write(rf_model)
